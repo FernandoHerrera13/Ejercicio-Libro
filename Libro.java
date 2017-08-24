080dda51e34c0c70a3d46446288a6b5be6aa081f
Fernando Herrera <poko_fernando@hotmail.com>
2017-08-24 11:05:12 -0500

    Se creo el metodo dameDetalles y se verifico su funcionamiento

--- Libro.java
+++ Libro.java
@@ -2,11 +2,13 @@ public class Libro
 {
     private String titulo;
     private String autor;
+    private int paginas;
     
-    public Libro(String titIni,String autIni)
+    public Libro(String titIni,String autIni,int numPag)
     {
         titulo=titIni;
         autor=autIni;
+        paginas=numPag;
     }
     
     public String dimeAutor()
@@ -17,6 +19,24 @@ public class Libro
     public String dimeLibro()
     {
         return titulo;
+    }
+    
+     public int dimePaginas()
+    {
+        return paginas;
+    }
+    
+    public String dimeDetalles()
+    {
+        String cadResultados="";
+        cadResultados+="AUTOR: ";
+        cadResultados+=autor;
+        cadResultados+="LIBRO: ";
+        cadResultados+=titulo;
+        cadResultados+="No.PAGINAS: ";
+        cadResultados+=paginas;
+        
+        return cadResultados;
     }
         
 }
