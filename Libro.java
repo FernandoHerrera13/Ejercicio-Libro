0d93ae1e4b8942ac1a416f90c0f1309e2511640a
Fernando Herrera <poko_fernando@hotmail.com>
2017-08-24 10:39:03 -0500

    Se verifico que el objeto se crea y se asigna el nombre y el autor

---
+++ Libro.java
@@ -0,0 +1,11 @@
+public class Libro
+{
+    private String titulo;
+    private String autor;
+    
+    public Libro(String titIni,String autIni)
+    {
+        titulo=titIni;
+        autor=autIni;
+    }
+}
