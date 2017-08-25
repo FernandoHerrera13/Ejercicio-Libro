2cbdc9c19e5e0d431d2d7ae66bda9fedd720543d
Fernando Herrera <poko_fernando@hotmail.com>
2017-08-25 15:55:31 -0500

    Se agregaron las modificaciones necesarias para modificar e incluir en la informacion el numero de referencia

--- Libro.java
+++ Libro.java
@@ -3,12 +3,24 @@ public class Libro
     private String titulo;
     private String autor;
     private int paginas;
+    private String numeroDeReferencia;
     
     public Libro(String titIni,String autIni,int numPag)
     {
         titulo=titIni;
         autor=autIni;
         paginas=numPag;
+        numeroDeReferencia="";
+    }
+    
+    public String dimeNumRef()
+    {
+        return numeroDeReferencia;
+    }
+    
+    public void cambiaNumRef(String NumRefInicial)
+    {
+        numeroDeReferencia=NumRefInicial;
     }
     
     public String dimeAutor()
@@ -35,6 +47,17 @@ public class Libro
         cadResultados+=titulo;
         cadResultados+="No.PAGINAS: ";
         cadResultados+=paginas;
+        
+        if(numeroDeReferencia.length()>0)
+        {
+            cadResultados+="Num.REFERENCIA: ";
+            cadResultados+=numeroDeReferencia;
+        }
+        else
+        {
+            cadResultados+="Num.REFERENCIA: ";
+            cadResultados+="ZZZ";
+        }
         
         return cadResultados;
     }
