b6467ad5cd8d50d0945b0983d37649157909e1a0
Fernando Herrera <poko_fernando@hotmail.com>
2017-09-02 18:56:55 -0500

    Se hizo la documentacion necesaria para el ejercicio

--- Libro.java
+++ Libro.java
@@ -6,30 +6,43 @@ public class Libro
     private String numeroDeReferencia;
     private int prestamos;
     
-    public Libro(String titIni,String autIni,int numPag)
+    /**El constructor inicializa las variables de instancia con la informacion necesaria
+     * @param titulo
+     * @param autor
+     * @param paginas
+     */
+    public Libro(String tiulo,String autor,int paginas)
     {
-        titulo=titIni;
-        autor=autIni;
-        paginas=numPag;
+        this.titulo=titulo;
+        this.autor=autor;
+        this.paginas=paginas;
         numeroDeReferencia="";
         prestamos=0;
     }
-    
+    /**Incrementa en uno los prestamos del libro*/
     public void prestar()
     {
         prestamos+=1;
     }
-    
+    /**Regresa la cantidad de veces que se ha prestado un numero
+       *return regresa un entero que representa las veces que se ha prestado un libro
+    */
     public int dimePrestamos()
     {
         return prestamos;
     }
     
+    /**Regresa el numero de referencia asociado al libro
+     * @return string representativo al numero de referencia asociado al libro
+     */
     public String dimeNumRef()
     {
         return numeroDeReferencia;
     }
     
+    /**Solicita al usuario el nuevo numero de referencia que va a asignar el libro
+     * @param NumRefInicial
+     */
     public void cambiaNumRef(String NumRefInicial)
     {
         if(NumRefInicial.length()>=3)
@@ -42,21 +55,33 @@ public class Libro
         }
     }
     
+    /**
+     * @return String con el nombre del autor
+     */
     public String dimeAutor()
     {
         return autor;
     }
     
+    /**
+     * @return String con el nombre del libro
+     */
     public String dimeLibro()
     {
         return titulo;
     }
     
+    /**
+     * @return int con el numero de paginas del libro
+     */
      public int dimePaginas()
     {
         return paginas;
     }
     
+    /**Muestra la concatenacion de todos los detalles del libro
+     * @return String con toda la informaicon del libro
+     */
     public String dimeDetalles()
     {
         String cadResultados="";
